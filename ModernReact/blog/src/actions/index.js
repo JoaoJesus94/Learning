4bc21877e1551f213b3bf6eb800ddccd1e8e0c59
jjesusxgeeks <j.jesus@xgeeks.io>
2019-09-12 10:47:10 +0100

    feat(Post List): Make use of redux to list values from posts

--- ModernReact/blog/src/actions/index.js
+++ ModernReact/blog/src/actions/index.js
@@ -3,5 +3,11 @@ import jsonPlaceholder from "../api/jsonPlaceholder";
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUsers = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/users');
+
+    dispatch({ type: "FETCH_USERS", payload: response.data})
+}
