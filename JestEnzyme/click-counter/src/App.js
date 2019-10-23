aa691dd8c52d8428f724cb84437bdd51ac8e5b58
jjesusxgeeks <j.jesus@xgeeks.io>
2019-10-23 16:41:21 +0100

    test(decrement button): add decrement button and tests

--- JestEnzyme/click-counter/src/App.js
+++ JestEnzyme/click-counter/src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      counter: 0
+      counter: 0,
+      error: null
     };
   }
   render() {
     return (
       <div data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
-      <button
-        data-test="increment-button"
-        onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        {this.state.error && (
+          <h3 data-test="error-display">Counter cannot be less than zero</h3>
+        )}
+        <button
+          data-test="increment-button"
+          onClick={() =>
+            this.setState({ counter: this.state.counter + 1, error: null })
+          }
         >
-        Increment counter
-      </button>
+          Increment counter
+        </button>
+        <button
+          data-test="decrement-button"
+          onClick={() =>
+            this.state.counter > 0
+              ? this.setState({ counter: this.state.counter - 1 })
+              : this.setState({ error: true })
+          }
+        >
+          Decrement counter
+        </button>
       </div>
     );
   }
--- JestEnzyme/click-counter/src/App.test.js
+++ JestEnzyme/click-counter/src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import App from './App';
+import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -12,12 +12,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @param {object} props - Component props specific to this setup.
  * @param {object} state - Initial state for setup.
  * @returns {ShallowWrapper}
-*/
-const setup = (props={}, state=null) => {
-  const wrapper = shallow(<App {...props} />)
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
   if (state) wrapper.setState(state);
   return wrapper;
-}
+};
 
 /**
  * Return ShallowWrapper containing node(s) with the given data-test value.
@@ -27,38 +27,94 @@ const setup = (props={}, state=null) => {
  */
 const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
-}
+};
 
-test('renders without error', () => {
+test("renders without error", () => {
   const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, 'component-app');
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
-test('renders increment button', () => {
+
+test("renders increment button", () => {
   const wrapper = setup();
-  const button = findByTestAttr(wrapper, 'increment-button');
+  const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
-test('renders counter display', () => {
+
+test("renders decrement button", () => {
   const wrapper = setup();
-  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  const button = findByTestAttr(wrapper, "decrement-button");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
-test('counter starts at 0', () => {
-  const wrapper = setup();
-  const initialCounterState = wrapper.state('counter');
-  expect(initialCounterState).toBe(0)
+
+test("render error message", () => {
+  const error = true;
+  const wrapper = setup(null, { error });
+  const errorDisplay = findByTestAttr(wrapper, "error-display");
+  expect(errorDisplay.length).toBe(1);
 });
-test('clicking button increments counter display', () => {
+
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
+
+test("clicking increment button increments counter display", () => {
   const counter = 7;
   const wrapper = setup(null, { counter });
 
   // find button and click
-  const button = findByTestAttr(wrapper, 'increment-button');
-  button.simulate('click');
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate("click");
 
   // find display and test value
-  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
-  expect(counterDisplay.text()).toContain(counter + 1)
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
 
+test("clicking decrement button decrement counter display", () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  // find button and click
+  const button = findByTestAttr(wrapper, "decrement-button");
+  button.simulate("click");
+
+  // find display and test value
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter - 1);
+});
+
+test("clicking increment when error is showing make error disappear", () => {
+  const error = true;
+  const wrapper = setup(null, { error });
+  let errorDisplay = findByTestAttr(wrapper, "error-display");
+  expect(errorDisplay.length).toBe(1);
+
+  //find button and click
+  const incrementWrapper = findByTestAttr(wrapper, 'increment-button');
+  incrementWrapper.simulate('click');
+
+  errorDisplay = findByTestAttr(wrapper, "error-display");
+  expect(errorDisplay.length).toBe(0);
+});
+
+test("clicking decrement when counter is zero to show error", () => {
+  const wrapper = setup();
+  let errorDisplay = findByTestAttr(wrapper, "error-display");
+  expect(errorDisplay.length).toBe(0);
+
+  //find button and click
+  const decrementWrapper = findByTestAttr(wrapper, 'decrement-button');
+  decrementWrapper.simulate('click');
+
+  errorDisplay = findByTestAttr(wrapper, "error-display");
+  expect(errorDisplay.length).toBe(1);
 });
